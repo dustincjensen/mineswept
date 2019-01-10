1af9428c3decf87b4836c6702dfd4116257b89c0
Dustin Jensen <dustincjensen@gmail.com>
2019-01-10 00:54:32 -0700

    Update comments on FontChange and HexToRgb

--- src/ui/FontChange.java
+++ src/ui/FontChange.java
@@ -1,12 +1,19 @@
 package ui;
 
 import java.awt.Font;
-
 import javax.swing.JComponent;
 
 public class FontChange {
-	public static void setFont(JComponent change, int size) {
-		Font f = change.getFont();
-		change.setFont(new Font(f.getFontName(), f.getStyle(), size));
+	/**
+	 * Set the font size of a JComponent.
+	 * 
+	 * @param component the component to update font size.
+	 * @param size the new font size to use.
+	 */
+	public static void setFont(JComponent component, int size) {
+		var font = component.getFont();
+		var name = font.getFontName();
+		var style = font.getStyle();
+		component.setFont(new Font(name, style, size));
 	}
 }
--- src/ui/HexToRgb.java
+++ src/ui/HexToRgb.java
@@ -3,6 +3,13 @@ package ui;
 import java.awt.Color;
 
 public class HexToRgb {
+	/**
+	 * Return a color object based on a 7 character hex code.
+	 * Example: #123456 including the #.
+	 * 
+	 * @param hexColorCode the color of the hex code.
+	 * @return the new color object.
+	 */
     public static Color convert(String hexColorCode) {
 		return new Color(
 			Integer.valueOf(hexColorCode.substring(1, 3), 16),
